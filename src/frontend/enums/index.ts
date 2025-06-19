86107bb2efc8a18144292db8bbf2b3b15988d0c1
Rnchg <Rnchg@hotmail.com>
2025-06-19 23:10:47 +0800

    Update V 2.0.6

--- src/frontend/enums/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./api/result.enum";
-
-export * from "./settings/layout.enum";
-export * from "./settings/theme.enum";
-export * from "./settings/locale.enum";
-export * from "./settings/device.enum";
